071c59ab8c002d3a46de3e084ea5cfca0a8af9c9
Sans172 <sandywildani170@gmail.com>
2021-03-31 11:50:47 +0700

    Add project files.

---
+++ Overloading Method/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overloading_Method
+{
+    class Numbers
+    {
+        public int Findminimum(int number1, int number2)
+        {
+            return number1 - number2 - number2;
+        }
+        public int Findminimum(int number1, int number2, float number3)
+        {
+            return number1 + (int)number3 - number1 - number2;
+        }
+        public int Findmaximum(int number1, int number2)
+        {
+            return number1 + number2 - number2;
+        }
+        public int Findmaximum(int number1, int number2, float number3)
+        {
+            return number1 + number2 + (int)number3 - number1 - number2;
+        }
+
+    }
+}
---
+++ Overloading Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Overloading_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1:  {0}", number.Findminimum(3, 1));
+            Console.WriteLine("Minimum #2:  {0}", number.Findminimum(3,2,4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1:  {0}", number.Findmaximum(3, 1));
+            Console.WriteLine("Maximum #2:  {0}", number.Findmaximum(3, 2, 4));
+
+            Console.ReadKey();
+
+        }
+    }
+}
